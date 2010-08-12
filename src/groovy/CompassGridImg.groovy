1391bea7fe02a290b8c65df1774e12b2b513cb21
David Padbury <david@davidpadbury.com>
2010-08-12 10:27:35 -0400

    Enabling config option for compass path

--- src/groovy/CompassGridImg.groovy
+++ src/groovy/CompassGridImg.groovy
@@ -3,8 +3,9 @@ class CompassGridImg {
 	
 	public static void make_grid_img(config, ant, dimensions) {
 		def images_dir = config.grass?.images_dir
+		def compass_path = config.grass?.compass_path ?: 'compass'
 
-		ant.exec(executable: "compass") {
+		ant.exec(executable: compass_path) {
 			arg(line: "--grid-img ${dimensions} --images-dir ${images_dir} --force")
 		}
 		
